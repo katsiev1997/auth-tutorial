6e5ef2ddda3918766078fae5851ef5efd1ad9fad
Mikail Katsiev <katsiev1997@gmail.com>
2024-08-17 14:25:40 +0300

    Card Wrapper

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export const LoginForm = () => {
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      Login Form!
+    </CardWrapper>
+  );
+};
+ 
